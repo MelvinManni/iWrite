7ff2191c92cfd40b29b8092e9d2f9ca2d1020c84
MelvinManni <melvinnnamani@gmail.com>
2020-01-29 12:13:11 +0100

    profile image correction

--- details.js
+++ details.js
@@ -23,4 +23,22 @@ closeOv = () => {
 }
 
 
+let image = document.getElementById('output');
+let cancel = document.getElementById('cancel');
 
+var loadFile = function (event) {
+    image.style.display = 'block';
+    image.src = URL.createObjectURL(event.target.files[0]);
+    document.getElementById('default').style.display = 'none';
+    document.getElementById('upload-text').innerHTML = 'Change Image';
+    cancel.style.visibility = 'visible';
+};
+
+cancel.addEventListener('click', ()=>{
+    image.src = '';
+    image.style.display = 'none';
+    document.getElementById('default').style.display = 'block';
+    document.getElementById('upload-text').innerHTML = 'Upload Image';
+    cancel.style.visibility = 'collapse'
+
+})
